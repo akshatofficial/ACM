ff08a2ae031e62a2ea6e8cd9d15c32235cd85a79
aashisha25 <iamaashisha@gmail.com>
2020-06-17 03:20:09 +0530

    About and Contact us section

--- app/src/main/java/com/example/acm/navigation_sponcors.java
+++ app/src/main/java/com/example/acm/navigation_sponcors.java
@@ -9,12 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class navigation_about extends Fragment {
+public class navigation_sponcors extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_about, container, false);
+        return inflater.inflate(R.layout.fragment_navigation_sponcors, container, false);
     }
 }
